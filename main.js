4a2dadecb9957e1fac35aba7a266b2adfb2d4809
bogorek <bogorek@spencerhealthsolutions.com>
2023-12-01 09:10:34 -0500

    deleted redundant image. Added X post option after score

--- main.js
+++ main.js
@@ -9,6 +9,13 @@ if ('serviceWorker' in navigator) {
   });
 }
 
+let score = 0; // Initialize score
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+});
+
 const triviaQuestions = [
     {
         question: "In which Christmas movie does the main character travel to New York to meet his father?",
@@ -22,26 +29,46 @@ const triviaQuestions = [
     }
 ];
 
+function generateShareUrl(score) {
+    const tweetText = `I scored ${score} points in the 12 Days of Christmas Trivia! Check it out: [Your Game URL] #12DaysTrivia`;
+    return `https://x.com/intent/tweet?text=${encodeURIComponent(tweetText)}`;
+}
 
-document.addEventListener('DOMContentLoaded', () => {                                                                                                
-    const submitButton = document.getElementById('submit');                                                                                          
-    const feedbackElement = document.getElementById('feedback');                                                                                     
+
+document.addEventListener('DOMContentLoaded', () => {
+    const submitButton = document.getElementById('submit');
+    const feedbackElement = document.getElementById('feedback');
 
     displayQuestionOfDay();
 
-    submitButton.onclick = (e) => {                                                                                                                  
-        e.preventDefault();                                                                                                                          
-        const day = getDayOfChristmas();
-        if (day !== null && day <= triviaQuestions.length) {
-            const dayTrivia = triviaQuestions[day - 1];
-            let selectedAnswer = document.querySelector('input[name="triviaOption"]:checked')?.value;                                                    
-            if (selectedAnswer === dayTrivia.answer) {                                                                                                      
-                feedbackElement.textContent = 'Correct!';                                                                                                
-            } else {                                                                                                                                     
-                feedbackElement.textContent = 'Try again.';                                                                                              
-            }
-        }                                                                                                                                            
-    };                                                                                                                                               
+  document.getElementById('shareButton').addEventListener('click', () => {
+      const shareUrl = generateShareUrl(score);
+      window.open(shareUrl, '_blank');
+  });
+
+  document.getElementById('submit').onclick = (e) => {
+      e.preventDefault();
+      console.log("Submit button clicked");
+      document.getElementById('shareButton').style.display = 'block';
+  };
+
+  submitButton.onclick = (e) => {
+      e.preventDefault();
+      const day = getDayOfChristmas();
+      if (day !== null && day <= triviaQuestions.length) {
+        const dayTrivia = triviaQuestions[day - 1];
+        let selectedAnswer = document.querySelector('input[name="triviaOption"]:checked')?.value;
+        // Immediate feedback portion
+        if (selectedAnswer === dayTrivia.answer) {
+            feedbackElement.textContent = 'Correct!';
+        } else {
+            feedbackElement.textContent = 'Try again.';
+        }
+        // Post to X
+        console.log("Submit button clicked");
+        document.getElementById('shareButton').style.display = 'block';
+      }
+  };
 });
 
 
