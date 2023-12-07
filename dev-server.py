033f20ebb00cc06d435a36dd8d689f3ee1142ce5
bogorek <bogorek@spencerhealthsolutions.com>
2023-12-07 08:06:31 -0500

    updated Dev Server

--- dev-server.py
+++ dev-server.py
@@ -1,18 +1,8 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 import os
 
-class CustomHandler(SimpleHTTPRequestHandler):
-    def translate_path(self, path):
-        """Translate URL paths to filesystem paths."""
-        # Route /XmasMovieTrivia to the current directory
-        if path.startswith('/XmasMovieTrivia'):
-            path = path[len('/XmasMovieTrivia'):]  # Remove the /XmasMovieTrivia part
-            if not path or path == '/':
-                path = '/index.html'  # Default to index.html
-        return os.path.join(os.getcwd(), path.lstrip('/'))
-
 PORT = 3000
 
-httpd = HTTPServer(('', PORT), CustomHandler)
-print(f"Serving at http://localhost:{PORT}/XmasMovieTrivia")
+httpd = HTTPServer(('', PORT), SimpleHTTPRequestHandler)
+print(f"Serving at http://localhost:{PORT}")
 httpd.serve_forever()
