016b69c90e87da1649b951c3f7e57709634843a5
bogorek <bogorek@spencerhealthsolutions.com>
2023-12-07 08:03:14 -0500

    trying without the root folder

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/XmasMovieTrivia/service-worker.js')
+    navigator.serviceWorker.register('/service-worker.js')
       .then(registration => {
         console.log('SW registered: ', registration);
       }).catch(registrationError => {
